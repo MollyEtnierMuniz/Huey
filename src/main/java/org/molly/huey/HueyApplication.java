6722ef94af13621044f36ebb5d92cfd4421c446f
Molly Etnier Muniz <7molly26@pm.me>
2022-09-20 10:45:11 -0500

    initial commit

---
+++ src/main/java/org/molly/huey/HueyApplication.java
@@ -0,0 +1,13 @@
+package org.molly.huey;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HueyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HueyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/molly/huey/HueyApplicationTests.java
@@ -0,0 +1,13 @@
+package org.molly.huey;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HueyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
